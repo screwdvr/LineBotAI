542db8fe60d27119dc35073001f51e06bff7b0ec
screwdvr <elu2016m@gmail.com>
2025-07-20 11:17:45 +0800

    Resolve conflict: use origin/main version of WebhookController

--- Controllers/WebhookController.cs
+++ Controllers/WebhookController.cs
@@ -1,26 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
-using Microsoft.Extensions.Options;
-
-[ApiController]
-[Route("api/[controller]")]
-public class WebhookController : ControllerBase
-{
-    private readonly LineBotOptions _options;
-
-    public WebhookController(IOptions<LineBotOptions> options)
-    {
-        _options = options.Value;
-    }
-
-    [HttpPost]
-    public IActionResult Post()
-    {
-        // 回傳 OK 讓 LINE 不重送
-        return Ok("Webhook received.");
-    }
-}
-=======
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using LineBotAI.Models;
@@ -54,4 +32,3 @@ namespace LineBotAI.Controllers
         }
     }
 }
->>>>>>> 97313cdee81834d2903d4d0dc7c1e2b0cdef0a08
